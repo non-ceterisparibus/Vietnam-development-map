c0d68a986fc9083f672dc2917d18a0494f6e5b99
Thu-Duong <duongthu1012@gmail.com>
2021-08-05 17:47:56 +0200

    update identityid

--- codes/plot.py
+++ codes/plot.py
@@ -9,29 +9,28 @@ import plotly.express as px
 import plotly.io as pio
 pio.renderers.default = 'browser'
 
-def plot_animation_frame_vietnamstate(df,geo_json, color, hover_data, title):
+def plot_animation_frame_vietnamstate(df, geo_json, color_data, title):
     """
     Function to plot choropleth interactive plot by year. 
         Args:
         -------
             df(DataFrame): Dataframe containing the data to be plotted.
-            color(column): DataFrame column name of the coloring variable.
-            hover_data(column): DataFrame column name of the outcome variable.
-            
+            color_data(column): DataFrame column name of the coloring variable.
+            geo_json 
         Returns:
         ---------
             plot.choropleth
-
     """
     fig = px.choropleth(
         df,
-        locations ="Code",
-        animation_frame = "year",
         geojson = geo_json,
-        color = color,
-        range_color=(min(df[color]), max(df[color])),
+        locations ="Code",
+        featureidkey="properties.Code",
+        animation_frame = "year",
+        color = color_data,
+        range_color=(min(df[color_data]), max(df[color_data])),
         hover_name = "Name",
-        hover_data = [hover_data],
+        # hover_data = [hover_data],
         title = title,
     )
     fig.update_geos(fitbounds = "locations", visible=False)
